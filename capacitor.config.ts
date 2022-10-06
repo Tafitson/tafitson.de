0c18d32c58771d182c9e890c47f21f73c1f9bf3b
David Thiele <david@sufftrupp.de>
2022-10-06 15:13:31 +0200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tafitson.de',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
